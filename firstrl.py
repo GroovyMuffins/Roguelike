08f47a26e1ad604b47ced23007ecc7ae33cade78
Alf B. Bertnum <bertnum@gmail.com>
2018-04-18 23:01:06 +0200

    Refactor Rect to classes module

--- firstrl.py
+++ firstrl.py
@@ -6,25 +6,7 @@ import shelve
 import support.variables as var
 import libtcodpy as libtcod
 from classes.Tile import Tile
-
-class Rect:
-    """a rectangle on the map. used to characterize a room."""
-    def __init__(self, x, y, width, height):
-        self.x1 = x
-        self.y1 = y
-        self.x2 = x + width
-        self.y2 = y + height
-
-    def center(self):
-        """return center coordinates of the room"""
-        center_x = (self.x1 + self.x2) / 2
-        center_y = (self.y1 + self.y2) / 2
-        return (center_x, center_y)
-
-    def intersect(self, other):
-        """returns true if this rectangle intersects wth another one"""
-        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
-                self.y1 <= other.y2 and self.y2 >= other.y1)
+from classes.Rect import Rect
 
 class Object:
     """This is generic object: the player, a monster, an tiem, the stairs...
