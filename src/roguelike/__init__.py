aca21b488419afa0be68238516504b703a11d0b5
Alf Bjørnar Bertnum <GroovyMuffins@users.noreply.github.com>
2024-03-24 20:45:34 +0100

    Fix init file

--- src/roguelike/__init__.py
+++ src/roguelike/__init__.py
@@ -1,2 +1,4 @@
-from classes import BasicMonster, ConfusedMonster, Equipment, Fighter, Item, Object, Rect, Tile
-from support import common, constants, variables
+"""Roguelike package."""
+
+from .classes import BasicMonster, ConfusedMonster, Equipment, Fighter, Item, Object, Rect, Tile
+from .support import common, constants, variables
