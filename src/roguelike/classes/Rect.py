ba9672325942e3799b6ad9ac77aab0a8bab7162a
Alf Bjørnar Bertnum <GroovyMuffins@users.noreply.github.com>
2024-03-24 21:04:22 +0100

    Ruff check fix

--- src/roguelike/classes/Rect.py
+++ src/roguelike/classes/Rect.py
@@ -17,11 +17,11 @@ class Rect:
         self.y2 = self.y1 + self.h
 
     def center(self):
-        """return center coordinates of the room"""
+        """Return center coordinates of the room"""
         center_x = (self.x1 + self.x2) / 2
         center_y = (self.y1 + self.y2) / 2
         return (center_x, center_y)
 
     def intersect(self, other):
-        """returns true if this rectangle intersects wth another one"""
+        """Returns true if this rectangle intersects wth another one"""
         return self.x1 <= other.x2 and self.x2 >= other.x1 and self.y1 <= other.y2 and self.y2 >= other.y1
